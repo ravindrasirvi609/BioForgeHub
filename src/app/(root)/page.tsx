edabe4341d4500b2ff5ded149c882c766a25c0de
Ravindra <sirviravindra609@gmail.com>
2023-12-26 22:28:14 +0530

    Add root layout and shared components

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-4xl">Operant Events</h1>
-      <Button variant="destructive">Click me</Button>
+
     </main>
   );
 }
