521d012bb907bc63c3cf822478d7fcec85002dc3
Ravindra <sirviravindra609@gmail.com>
2023-12-26 21:57:32 +0530

    Add new files and configurations

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Operant Events</h1>
+      <Button variant="destructive">Click me</Button>
+    </main>
+  );
+}
