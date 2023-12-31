f2a78581813203f3f9af3a9c918ef9195c4500b7
Ravindra <sirviravindra609@gmail.com>
2023-12-31 20:20:13 +0530

    Refactor home page and comment out unused code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,65 @@
-import { Button } from "../../components/button";
-import Image from "next/image";
+// import CategoryFilter from '@/components/shared/CategoryFilter';
+// import Collection from '@/components/shared/Collection'
+// import Search from '@/components/shared/Search';
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '../../components/button';
+import { SearchParamProps } from '../../types';
 
-export default function Home() {
-  return <main>
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Image src="/assets/images/logo.svg" alt="BioForgeHub Logo" width={128} height={38} />
-      <Button asChild className="rounded-full" size="lg">
-        <a href="/auth/sign-in">Login</a>
-      </Button>
-    </div>
-  </main>;
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
+  // const events = await getAllEvents({
+  //   query: searchText,
+  //   category,
+  //   page,
+  //   limit: 6
+  // })
+
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
+            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">
+                Explore Now
+              </Link>
+            </Button>
+          </div>
+
+          <Image 
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
+        </div>
+      </section> 
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          {/* <Search />
+          <CategoryFilter /> */}
+        </div>
+
+        {/* <Collection 
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
+        /> */}
+      </section>
+    </>
+  )
 }
