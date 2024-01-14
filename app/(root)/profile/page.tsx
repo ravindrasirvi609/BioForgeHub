0f0cd983fe26dd9c950ca334e01fb7775da03479
Ravindra <sirviravindra609@gmail.com>
2024-01-14 21:29:51 +0530

    Add UI components and update API calls in ProfilePage and create OrdersPage

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 import React from "react";
 import { SearchParamProps } from "../../../types";
 import { IOrder } from "../../../lib/database/models/order.model";
-import { Button } from "../../../components/button";
+import { Button } from "../../../components/ui/button";
+import Collection from "../../../components/shared/Collection";
+import { getOrdersByUser } from "../../../lib/action/order.actions";
+import { getEventsByUser } from "../../../lib/action/event.actions";
 
 const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
@@ -12,10 +15,10 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const ordersPage = Number(searchParams?.ordersPage) || 1;
   const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-  //const orders = await getOrdersByUser({ userId, page: ordersPage})
+  const orders = await getOrdersByUser({ userId, page: ordersPage})
 
-  // const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
-  // const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
+   const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+   const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
 
   return (
     <>
@@ -30,7 +33,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
       </section>
 
       <section className="wrapper my-8">
-        {/* <Collection 
+        <Collection 
           data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
           emptyStateSubtext="No worries - plenty of exciting events to explore!"
@@ -39,7 +42,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
           page={ordersPage}
           urlParamName="ordersPage"
           totalPages={orders?.totalPages}
-        /> */}
+        />
       </section>
 
       {/* Events Organized */}
@@ -53,7 +56,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
       </section>
 
       <section className="wrapper my-8">
-        {/* <Collection 
+        <Collection 
           data={organizedEvents?.data}
           emptyTitle="No events have been created yet"
           emptyStateSubtext="Go create some now"
@@ -62,7 +65,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
           page={eventsPage}
           urlParamName="eventsPage"
           totalPages={organizedEvents?.totalPages}
-        /> */}
+        />
       </section>
     </>
   );
