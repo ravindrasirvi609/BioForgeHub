24f65ec79cf5cf81f7808245269556db4b890548
Ravindra <sirviravindra609@gmail.com>
2024-02-05 23:28:22 +0530

    Checkout tsx = new TransactionCheckouts

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,21 +1,23 @@
-import React, { useEffect } from 'react'
-import { loadStripe } from '@stripe/stripe-js';
-import { IEvent } from '../../lib/database/models/event.model';
-import { Button } from '../ui/button';
-import { checkoutOrder } from '../../lib/action/order.actions';
+import React, { useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { IEvent } from "../../lib/database/models/event.model";
+import { Button } from "../ui/button";
+import { checkoutOrder } from "../../lib/action/order.actions";
 
 loadStripe(process.env.RAZORPAY_KEY!);
 
-const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
+const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
   useEffect(() => {
     // Check to see if this is a redirect back from Checkout
     const query = new URLSearchParams(window.location.search);
-    if (query.get('success')) {
-      console.log('Order placed! You will receive an email confirmation.');
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
     }
 
-    if (query.get('canceled')) {
-      console.log('Order canceled -- continue to shop around and checkout when you’re ready.');
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
     }
   }, []);
 
@@ -25,19 +27,19 @@ const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
       eventId: event._id,
       price: event.price,
       isFree: event.isFree,
-      buyerId: userId
-    }
+      buyerId: userId,
+    };
 
     await checkoutOrder(order);
-  }
+  };
 
   return (
     <form action={onCheckout} method="post">
       <Button type="submit" role="link" size="lg" className="button sm:w-fit">
-        {event.isFree ? 'Get Ticket' : 'Buy Ticket'}
+        {event.isFree ? "Get Ticket" : "Buy Ticket"}
       </Button>
     </form>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
